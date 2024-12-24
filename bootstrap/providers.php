c0cf69c9f2078e13efd00a90c25147bffa128a7a
Daniele Montecchi <daniele@montecchi.me>
2024-12-24 01:41:22 +0100

    Implemented base template

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
 	App\Providers\AppServiceProvider::class,
 	App\Providers\Filament\AdminPanelProvider::class,
 	Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+	BladeUI\Heroicons\BladeHeroiconsServiceProvider::class,
 ];
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,23 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1 class="text-4xl font-bold text-gray-800">About Us</h1>
-    <p class="mt-4 text-gray-600">
-        Learn more about our company and mission. We aim to provide quality content to our readers.
-    </p>
-@endsection
+<x-layouts.app>
+    <div class="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+        <div class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+             aria-hidden="true"></div>
+        <div class="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-8 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+                <!-- <h1 class="max-w-2xl text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto">We’re changing the way people connect</h1> -->
+                <h1 class="max-w-2xl text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto">
+                    About page</h1>
+                <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+                    <p class="text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Anim aute id magna aliqua ad
+                        ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                        occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+                        cupidatat commodo.</p>
+                </div>
+                <img src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
+                     alt=""
+                     class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36">
+            </div>
+        </div>
+        <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div>
+    </div>
+</x-layouts.app>
